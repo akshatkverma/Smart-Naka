085243ef71c050b6c12ead18fbb1d01cf230785e
Akshat Kumar Verma <akshatvermajbp@gmail.com>
2023-01-02 23:38:18 +0530

    Made Basic Layout for HomeFragment.kt

--- app/src/main/java/ethos/lifetime/smartnaka/HomeFragment.kt
+++ app/src/main/java/ethos/lifetime/smartnaka/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import ethos.lifetime.smartnaka.databinding.FragmentHomeBinding
 
@@ -18,6 +19,13 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.textField1.setEndIconOnClickListener {
+            Toast.makeText(requireContext(), "Button Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
--- app/src/main/java/ethos/lifetime/smartnaka/MainActivity.kt
+++ app/src/main/java/ethos/lifetime/smartnaka/MainActivity.kt
@@ -5,10 +5,6 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        application.setTheme(R.style.Theme_SmartNaka)
-        setTheme(R.style.Theme_SmartNaka)
-
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
